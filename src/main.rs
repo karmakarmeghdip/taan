ebae9eb7b1eb8ac2193c3fa2f79f21dbac76558c
karmakarmeghdip <karmakarmeghdip@gmail.com>
2025-09-17 16:06:09 +0530

    feat: implemented header bar

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,30 @@ mod spotify;
 mod state;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let rt = setup_rt()?;
+    let spot = rt.block_on(async { spotify::SpotifyState::default() });
+    let ui = MainWindow::new()?;
+    // {
+    //     ui.on_clicked(move || {
+    //         let spot = spot.clone();
+    //         println!("Attempting to authenticate...");
+    //         rt.spawn(async move {
+    //             spot.auth().await.unwrap();
+    //             println!("Auth successful");
+    //         });
+    //     });
+    // }
+    ui.run()?;
+    Ok(())
+}
+
+fn setup_rt() -> tokio::io::Result<tokio::runtime::Handle> {
     let rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()?;
-    let spot = rt.block_on(async { spotify::SpotifyState::default() });
     let rt_handle = rt.handle().clone();
     std::thread::spawn(move || {
         rt.block_on(std::future::pending::<()>());
     });
-    let ui = MainWindow::new()?;
-    {
-        ui.on_clicked(move || {
-            let spot = spot.clone();
-            println!("Attempting to authenticate...");
-            rt_handle.spawn(async move {
-                spot.auth().await.unwrap();
-                println!("Auth successful");
-            });
-        });
-    }
-    ui.run()?;
-    Ok(())
+    Ok(rt_handle)
 }
